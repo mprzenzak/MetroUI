ae01820e0be398f67662eca19b628f6c479ff1f4
mprzenzak <mprzenzak@gmail.com>
2018-03-30 22:49:37 +0200

    Attempts to add text to tiles

--- Basic version/MetroUI 03'2018 basic.js
+++ Basic version/MetroUI 03'2018 basic.js
@@ -1,6 +1,6 @@
 //var title = "testinputa";
 //document.getElementById('titleInput').value;
-function createGrid(x,y) {
+function createGrid(x, y) {
     for (var cols = 0; cols < x; cols++) {
         for (var rows = 0; rows < y; rows++) {
             $('#container').append("<div class='grid'></div>");
@@ -12,11 +12,34 @@ function createGrid(x,y) {
 function refreshGrid() {
     var x = document.getElementById('colsNumber').value;
     var y = document.getElementById('rowsNumber').value;
+    var count = x * y;
+
     $('.grid').remove();
-    createGrid(x,y);
+    createGrid(x, y);
 };
 $(document).ready(function () {
     $(".startBtn").click(function () {
         refreshGrid();
     });
 });
+function getData(count) {
+    var names = ["Michał", "Jan", "Katarzyna", "Andrzej", "Józef", "Bartek", "Mikołaj", "Tomasz", "Julian", "Brajan", "Dżesika"];
+    var surnames = ["Noga", "Kowalski", "Nowak", "Pazura", "Duda", "Komorowski", "Tomczyk", "Józefowicz", "Lechicki", "Goldberg"];
+    var result = [];
+
+    for (var i = 0; i < count; i++) {
+        var randomNameIndex = Math.floor(Math.random() * names.length);
+        var randomSurnameIndex = Math.floor(Math.random() * surnames.length);
+        var name = names[randomNameIndex];
+        var surname = surnames[randomSurnameIndex];
+        result.push({
+            name: name,
+            surname: surname
+        });
+        //$("grid").css("content", "500");////////////////////////////////////////////////////////////////////////////////////////////////////
+        var a ="test";
+        $("data-name") = a;
+    }
+
+    return result
+}
