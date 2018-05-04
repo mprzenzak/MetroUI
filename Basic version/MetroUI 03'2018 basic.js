e6d0fce75fcc93b5392fd179dbb2d32b1c50c6de
mprzenzak <mprzenzak@gmail.com>
2018-05-04 17:43:05 +0200

    Displaying username in additional div

--- Basic version/MetroUI 03'2018 basic.js
+++ Basic version/MetroUI 03'2018 basic.js
@@ -1,17 +1,18 @@
-//var title = "testinputa";
-//document.getElementById('titleInput').value;
 function createGrid(x, y) {
     for (var cols = 0; cols < x; cols++) {
         for (var rows = 0; rows < y; rows++) {
-            $('#container').append("<div class='grid'></div>");
+            $('#container').append("<div class='grid'>\
+            <div class='nameDisplay'>defaultUsername<span id='usernameSpan'> unchanged</span></div>\
+            </div>");
+            //$('.nameDisplay').append('#container');
         };
     };
     $('.grid').width(800 / x);
     $('.grid').height(800 / x);
 };
 function refreshGrid() {
-    var x = document.getElementById('colsNumber').value;
-    var y = document.getElementById('rowsNumber').value;
+    var x = $("#colsNumber")[0].value;
+    var y = $("#rowsNumber")[0].value;
     var count = x * y;
 
     $('.grid').remove();
@@ -23,23 +24,30 @@ $(document).ready(function () {
     });
 });
 function getData(count) {
-    var names = ["Michał", "Jan", "Katarzyna", "Andrzej", "Józef", "Bartek", "Mikołaj", "Tomasz", "Julian", "Brajan", "Dżesika"];
-    var surnames = ["Noga", "Kowalski", "Nowak", "Pazura", "Duda", "Komorowski", "Tomczyk", "Józefowicz", "Lechicki", "Goldberg"];
+    var names = ["Michal", "Jan", "Katarzyna", "Andrzej", "Jozef", "Bartek", "Mikolaj", "Tomasz", "Julian", "Brajan", "Dzesika"];
+    var surnames = ["Noga", "Kowalski", "Nowak", "Pazura", "Duda", "Komorowski", "Tomczyk", "Jozefowicz", "Lechicki", "Goldberg"];
     var result = [];
 
     for (var i = 0; i < count; i++) {
         var randomNameIndex = Math.floor(Math.random() * names.length);
         var randomSurnameIndex = Math.floor(Math.random() * surnames.length);
-        var name = names[randomNameIndex];
+        name = names[randomNameIndex];
         var surname = surnames[randomSurnameIndex];
         result.push({
             name: name,
             surname: surname
         });
-        //$("grid").css("content", "500");////////////////////////////////////////////////////////////////////////////////////////////////////
-        var a ="test";
-        $("data-name") = a;
     }
-
     return result
 }
+function textDisplay() {
+    var data = getData(1);
+    var username = "";
+    $.each(data, function (i, { name, surname }) {
+        username += ` ${name} ${surname}`;
+    });
+    $(".nameDisplay").html(username);
+}
+//$('.nameDisplay').innerHTML = changedUsername;
+//$('#usernameSpan').text('changedUsername');
+//$('#usernameSpan').html('changedUsername');
